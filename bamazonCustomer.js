6d4f806900badf19d8aaac75055716070b38119c
Ashish Dommety <ashishuiux@gmail.com>
2017-07-25 14:02:25 -0700

    customer functionality complete!

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -15,42 +15,38 @@ connection.connect(function(err) {
 });
 
 var query = connection.query("SELECT * FROM products", function(err, res) {
-  var choices = [];
-  console.log("-----------Available Products-----------");
-  for (var i = 0; i < res.length; i++) {
-    choices.push(`${res[i].item_id} - ${res[i].product_name} - $${res[i].price}`);
-    console.log(`${res[i].item_id} - ${res[i].product_name} - $${res[i].price}`);
-  }
-
-  inquirer.prompt([{
-      type: "input",
-      message: "What is the ID of the item you want to buy?",
-      name: "itemId"
-    },
-    {
-      type: "input",
-      message: "How many do you want to buy?",
-      name: "itemQuantity"
-    }
-  ]).then(function(answer) {
-    connection.query("SELECT * FROM products WHERE item_id=?",[answer.itemId], function(err,res){
-      if(res[0].stock_quantity >= answer.itemQuantity){
-        
-        console.log("purchase successful!");
+      var choices = [];
+      console.log("-----------Available Products-----------");
+      for (var i = 0; i < res.length; i++) {
+        choices.push(`${res[i].item_id} - ${res[i].product_name} - $${res[i].price}`);
+        console.log(`${res[i].item_id} - ${res[i].product_name} - $${res[i].price}`);
       }
-      else{
-        console.log("Insufficient quantity");
-      }
-      // console.log(res[0].stock_quantity);
-    })
-    // NOTE: compare the id to find the item, and then check it's quantity
-  });
 
-  // console.log(choices);
-
-  // console.log(res);
-});
-
-// NOTE: prompt id of product, and then prompt the quantity.
-// NOTE: once the input is given, check the quantity and check if there's enough.
-// NOTE: if there is, deplete stock, or else show "insufficient quantity"
+      inquirer.prompt([{
+          type: "input",
+          message: "What is the ID of the item you want to buy?",
+          name: "itemId"
+        },
+        {
+          type: "input",
+          message: "How many do you want to buy?",
+          name: "itemQuantity"
+        }
+      ]).then(function(answer) {
+          connection.query("SELECT * FROM products WHERE item_id=?", [answer.itemId], function(err, res) {
+              if (res[0].stock_quantity >= answer.itemQuantity) {
+                connection.query("UPDATE products SET ? WHERE ?",
+                  [{
+                      stock_quantity: res[0].stock_quantity - parseInt(answer.itemQuantity)
+                    },
+                    {
+                      item_id: answer.itemId
+                    }
+                  ], function(error){
+                    if(error) throw err;
+                    console.log(`You just bought ${answer.itemQuantity} ${res[0].product_name}s worth $${res[0].price * answer.itemQuantity}`);
+                  });
+                }
+              });
+          });
+      });
